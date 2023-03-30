ea3ff4017b140907fc088af47d62a9720cc02676
MateoCastanoG <m.castano1@uniandes.edu.co>
2023-03-30 16:29:27 -0500

    🚧[wip]Moving the square on the screen

--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -26,10 +26,21 @@ class GameEngine:
         self._clean()
 
     def _create(self):
-        pass
+        """Create the velocity"""
+        self.vel_cuad = pygame.Vector2(50, 50)
+        """Create the game objects"""
+        self.pos_cuad = pygame.Vector2(0, 0)
+        size_cuad = pygame.Vector2(50, 50)
+        col_cuad = pygame.Color(255, 255, 255)
+
+        """Create surface"""
+        self.surf_cuad = pygame.Surface(size_cuad)
+        """Fill surface"""
+        self.surf_cuad.fill(col_cuad)
 
     def _calculate_time(self):
-        pass
+        """Calculate delta time"""
+        self.delta_time = self.clock.tick(self.framerate) / 1000
 
     def _process_events(self):
         for event in pygame.event.get():
@@ -37,11 +48,29 @@ class GameEngine:
                 self.is_running = False
 
     def _update(self):
-        pass
+        self.pos_cuad.x += self.vel_cuad.x * self.delta_time
+        self.pos_cuad.y += self.vel_cuad.y * self.delta_time
+
+        """Check if the square is out of the screen"""
+        screen_rect = self.screen.get_rect()
+        cuad_rect = self.surf_cuad.get_rect(topleft=self.pos_cuad)
+
+        if cuad_rect.left <= 0 or cuad_rect.right >= screen_rect.width:
+            self.vel_cuad.x *= -1
+            cuad_rect.clamp_ip(screen_rect)
+            self.pos_cuad.x = cuad_rect.x
+
+        if cuad_rect.top <= 0 or cuad_rect.bottom >= screen_rect.height:
+            self.vel_cuad.y *= -1
+            cuad_rect.clamp_ip(screen_rect)
+            self.pos_cuad.y = cuad_rect.y
 
     def _draw(self):
         """Color the screen"""
-        self.screen.fill((0, 200, 128))
+        self.screen.fill((0, 0, 0))
+
+        """Draw the surface"""
+        self.screen.blit(self.surf_cuad, self.pos_cuad)
         """Clean the screen"""
         pygame.display.flip()
 
